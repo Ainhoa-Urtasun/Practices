4864bc24e6d7482335e920fb5887bb70ea5a801d
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-05-04 07:35:30 +0200

    Update Paychecks.py

--- Paychecks.py
+++ Paychecks.py
@@ -17,5 +17,5 @@ def Paychecks(Pay):
 
   plt.xlabel('Employees',fontsize=16)
   plt.title('Paychecks',fontsize=16)
-  plt.legend(ncols=4,bbox_to_anchor=(0,1.02,1,0.2),loc="lower left",fontsize='x-small')
+  plt.legend(ncols=4,bbox_to_anchor=(1.04,1),borderaxespad=0,fontsize='x-small')
   plt.show()
