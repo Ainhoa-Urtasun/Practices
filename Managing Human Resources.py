a53e43f0b8e7adfd32dec3f4130b3ee3ae876e1e
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-05-18 12:11:13 +0200

    Update Managing Human Resources.py

--- Managing Human Resources.py
+++ Managing Human Resources.py
@@ -2,7 +2,7 @@ import numpy
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-def ManagingHumanResources(Decisions):
+def ManagingHumanResources(Gift-exchange game):
   fig = plt.figure(figsize=(20,5))
   heights = 5*[1]
   widths = 4*[1]
