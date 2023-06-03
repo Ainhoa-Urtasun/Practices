8c06830037a8a0b0523a9ab027571be9f19f1281
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-06-03 13:50:10 +0200

    Update Practices.py

--- Practices.py
+++ Practices.py
@@ -2,7 +2,7 @@ import numpy
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-def Practice(game):
+def Practices(game):
   fig = plt.figure(figsize=(20,5))
   heights = 5*[1]
   widths = 4*[1]
