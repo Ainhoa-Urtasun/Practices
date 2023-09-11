29717eef18ec450f7e3a30d3dd9d2bcb9f70ea16
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-09-11 16:44:18 +0200

    Update Practice-1.py

--- Practice-1.py
+++ Practice-1.py
@@ -6,3 +6,12 @@ fixed = 'https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/'
 datasetCode = 'qoe_ewcs_7b3'
 url = '{}{}'.format(fixed,datasetCode)
 data = requests.get(url).json()
+
+dimensions = [pandas.DataFrame({key:val for key,val in data['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in data['id']]
+values = pandas.Series(data['value']).rename(index=int).sort_index()
+for num in data['size']:
+  result *= num
+
+values = values.reindex(range(0,result),fill_value=0)
+values.index = pandas.MultiIndex.from_product(dimensions,names=data['id'])
+values.unstack('geo')
