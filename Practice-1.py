b47d5a6aaa78564fd90f80ae2af9c863364904f7
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-09-14 08:57:08 +0200

    Update Practice-1.py

--- Practice-1.py
+++ Practice-1.py
@@ -1,18 +1,20 @@
-import requests
+aimport requests
 import json
 import pandas
 
-def statistics(id,country):
+def statistics(treelabel,country):
   fixed = 'https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/'
-  url = '{}{}'.format(fixed,id)
-  data = requests.get(url).json()
+  url = '{}{}'.format(fixed,treelabel)
+  metadata = requests.get(url).json()
+  print(metadata)
+  print(metadata['id'])
+  print(metadata['value'])
+  data = pandas.Series(metadata['value']).rename(index=int).sort_index()
   print(data)
-  df = pandas.Series(data['value']).rename(index=int).sort_index()
-  print(df)
   result = 1 # Initialize the result to 1
-  for num in data['size']:
-    result *= num
-  v = v.reindex(range(0,result),fill_value=0)
-  dimensions = [pandas.DataFrame({key:val for key,val in data['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in data['id']]
-  v.index = pandas.MultiIndex.from_product(dimensions,names=data['id'])
+  for num in metadata['size']:
+    n *= num
+  data = data.reindex(range(0,n),fill_value=0)
+  structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
+  data.index = pandas.MultiIndex.from_product(dimensions,names=metadata['id'])
   v.unstack('geo')[[country]]
