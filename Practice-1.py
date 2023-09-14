09d886e738007012e270e4f2195e221424ac43d3
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-09-14 09:01:59 +0200

    Update Practice-1.py

--- Practice-1.py
+++ Practice-1.py
@@ -12,5 +12,5 @@ def statistics(treelabel,country):
     n *= num
   data = data.reindex(range(0,n),fill_value=0)
   structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
-  data.index = pandas.MultiIndex.from_product(dimensions,names=metadata['id'])
-  v.unstack('geo')[[country]]
+  data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
+  data.unstack('geo')[[country]]
