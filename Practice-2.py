4de299c93a070d6d51ab150cc942e036c8d6f8cb
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-09-15 12:05:52 +0200

    Update Practice-2.py

--- Practice-2.py
+++ Practice-2.py
@@ -2,7 +2,7 @@ import requests
 import json
 import pandas
 
-def statistics(treelabel,country,unit,Total):
+def statistics(treelabel,country,unit):
   fixed = 'https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/'
   url = '{}{}'.format(fixed,treelabel)
   metadata = requests.get(url).json()
