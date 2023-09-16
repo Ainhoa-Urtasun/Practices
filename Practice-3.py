00872e407ebfa9446bed2cd42c1c993079c434e2
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-09-16 09:46:07 +0200

    Update Practice-3.py

--- Practice-3.py
+++ Practice-3.py
@@ -13,7 +13,8 @@ def data(treelabel,country1,country2,unit):
     n *= num
   data = data.reindex(range(0,n),fill_value=0)
   structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
-  data.index = pandas.MultiIndex.from_product(structure,names=metadata['id']).reset_index()
+  data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
+  data = data.reset_index()
   #data = data[(data.geo==country1)|(data.geo==country2)]
   #data = data[data['indic_is']==unit]
   #data['time'] = data['time'].astype(int)
