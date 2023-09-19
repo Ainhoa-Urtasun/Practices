0d24fa7947c7aff7d8e94a2d2dfcf22df15a0738
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-09-19 12:37:54 +0200

    Update Practice-4.py

--- Practice-4.py
+++ Practice-4.py
@@ -3,5 +3,5 @@ from wordcloud import WordCloud
 def wordcloud(mydict):
   wc = WordCloud(background_color='white').generate_from_frequencies(frequencies=mydict)
   fig = plt.figure(figsize=(10,5))
-  return plt.show(wc)
+  return plt.imshow(wc)
 
