451e71c961e97e39b1d254d76e417eec7bfd89c9
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-09 15:39:45 +0200

    Update Practice-1.py

--- Practice-1.py
+++ Practice-1.py
@@ -2,20 +2,19 @@ import requests
 import json
 import pandas
 
-def data(treelabel,country):
-  fixed = 'https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/'
-  url = '{}{}'.format(fixed,treelabel)
-  metadata = requests.get(url).json()
-  print(metadata['label'])
-  data = pandas.Series(metadata['value']).rename(index=int).sort_index()
-  n = 1 # Initialize the result to 1
-  for num in metadata['size']:
-    n *= num
-  data = data.reindex(range(0,n),fill_value=0)
-  structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
-  data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
-  data = data.reset_index()
-  data = data[data.geo=='Spain']
-  data = data[['age','sex','time',0]]
-  data.rename(columns={0:'percentage'},inplace=True)
-  return data
+fixed = 'https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/'
+url = '{}{}'.format(fixed,'qoe_ewcs_7b3')
+metadata = requests.get(url).json()
+print(metadata['label'])
+data = pandas.Series(metadata['value']).rename(index=int).sort_index()
+n = 1 # Initialize the result to 1
+for num in metadata['size']:
+  n *= num
+data = data.reindex(range(0,n),fill_value=0)
+structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
+data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
+data = data.reset_index()
+data = data[data.geo=='Spain']
+data = data[['age','sex','time',0]]
+data.rename(columns={0:'percentage'},inplace=True)
+return data
