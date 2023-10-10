b02bc241ff894ab9504b641699abdf00f11ce5ff
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-10 22:24:43 +0200

    Update PRACTICE-1.py

--- PRACTICE-1.py
+++ PRACTICE-1.py
@@ -24,7 +24,7 @@ structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]
 data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
 data = data.reset_index()
 data = data[data.sex=='Total']
-data = data[data.age=='From 15 to 24 years']
+data = data[data.age=='From 25 to 54 years']
 data = data[['geo','time',0]]
 data.rename(columns={0:'percentage'},inplace=True)
 
@@ -40,6 +40,3 @@ fig,ax = plt.subplots(1,figsize=(10,15))
 mydata.plot(column='percentage',alpha=0.8,cmap='viridis',ax=ax,legend=True)
 ax.set_title('Percentage of employed persons from 15 to 24 years\nthinking they do useful work (source: Eurofound)')
 ax.axis('off')
-ax.text(mydata.loc[mydata.ADMIN=='Spain','geometry'].centroid.x,mydata.loc[mydata.ADMIN=='Spain','geometry'].centroid.y,
-        str(data.loc[(data.ADMIN=='Spain')&(data.time=='2005'),'percentage'].values[0])+'% (2005)\n'+
-        str(data.loc[(data.ADMIN=='Spain')&(data.time=='2015'),'percentage'].values[0])+'% (2015)',fontsize=12,ha='center',va='center')
