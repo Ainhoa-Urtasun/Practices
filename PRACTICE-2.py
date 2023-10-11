9acdcccfb5f9d035c9bdb77657f6c8be93aa2fc2
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-11 15:36:41 +0200

    Update PRACTICE-2.py

--- PRACTICE-2.py
+++ PRACTICE-2.py
@@ -20,7 +20,6 @@ data = data.reindex(range(0,n),fill_value=0)
 structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
 data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
 mydata = data.reset_index()
-print(mydata)
 mydata = mydata[mydata.time=='2022']
 mydata = mydata[mydata['prof_pos']=='Executives']
 mydata = mydata[['geo',0]]
@@ -36,4 +35,4 @@ mydata = geopandas.GeoDataFrame(mydata,geometry='geometry')
 fig,ax = plt.subplots(1,figsize=(10,15))
 mydata.plot(column='Percentage',alpha=0.8,cmap='viridis',ax=ax,legend=True)
 ax.set_title('Percentage of women in senior management positions in 2022 (source: Eurostat)')
-ax.axes('off')
+ax.axis('off')
