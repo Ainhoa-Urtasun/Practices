ed454495b062090e90a3a715d0c354b699f17b47
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-11 15:55:17 +0200

    Update PRACTICE-3.py

--- PRACTICE-3.py
+++ PRACTICE-3.py
@@ -20,6 +20,7 @@ data = data.reindex(range(0,n),fill_value=0)
 structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
 data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
 mydata = data.reset_index()
+print(mydata)
 mydata = mydata[mydata.time=='2022']
 mydata = mydata[mydata.industry=='Professional, scientific and technical activities']
 mydata = mydata[mydata['digital intensity'].str.contains('Enterprises with very high digital intensity')]
