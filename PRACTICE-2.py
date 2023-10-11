bf0244a43e8790ece5ef94eea49fc81d6f4ee64e
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-11 06:55:36 +0200

    Update PRACTICE-2.py

--- PRACTICE-2.py
+++ PRACTICE-2.py
@@ -22,20 +22,19 @@ for num in metadata['size']:
 data = data.reindex(range(0,n),fill_value=0)
 structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
 data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
-data = data.reset_index()
-data['time'] = data['time'].astype(int)
-data = data[data.time>2009]
-data = data[data.age=='From 25 to 54 years']
-data = data[['sex','time',0]]
+mydata = data.reset_index()
+mydata['time'] = mydata['time'].astype(int)
+mydata = mydata[mydata.time>2009]
+mydata = mydata[mydata.age=='From 25 to 54 years']
+mydata = mydata[['sex','time',0]]
 data.rename(columns={0:'Thousand persons'},inplace=True)
+data.rename(columns={'geo':'ADMIN'},inplace=True)
 
 world = geopandas.read_file('/content/drive/MyDrive/2024-HRM/ne_110m_admin_0_countries.zip')[['ADMIN','geometry']]
 polygon = Polygon([(-25,35),(40,35),(40,75),(-25,75)])
 europe = geopandas.clip(world,polygon)
 
-data.rename(columns={'geo':'ADMIN'},inplace=True)
-mydata = data.copy()
 mydata = mydata.merge(europe,on='ADMIN',how='right')
 mydata = geopandas.GeoDataFrame(mydata,geometry='geometry')
 fig,ax = plt.subplots(1,figsize=(10,15))
-mydata.plot(column='percentage',alpha=0.8,cmap='viridis',ax=ax,legend=True)
+mydata.plot(column='Thousand persons',alpha=0.8,cmap='viridis',ax=ax,legend=True)
