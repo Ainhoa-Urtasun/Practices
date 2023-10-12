3611971a5654631835b76e7293e11f6041ae1025
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-12 21:50:08 +0200

    Update PRACTICE-1.py

--- PRACTICE-1.py
+++ PRACTICE-1.py
@@ -14,7 +14,7 @@ warnings.filterwarnings("ignore")
 fixed = 'https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/'
 url = '{}{}'.format(fixed,'lfso_21jsat01')
 metadata = requests.get(url).json()
-#print(metadata['label'])
+print(metadata['label'])
 data = pandas.Series(metadata['value']).rename(index=int).sort_index()
 n = 1 # Initialize the result to 1
 for num in metadata['size']:
@@ -23,6 +23,7 @@ data = data.reindex(range(0,n),fill_value=0)
 structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
 data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
 mydata = data.reset_index()
+print(mydata)
 mydata = mydata[mydata.sex=='Total']
 mydata = mydata[mydata['c_birth']=='Total']
 mydata = mydata[mydata['isced11']=='Total']
