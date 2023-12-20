529663e2d5af172c425cb250ece4f47ba71baf5a
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-20 07:29:27 +0100

    Update LOP 1-1.py

--- LOP 1-1.py
+++ LOP 1-1.py
@@ -1,47 +1,23 @@
-import requests
-import json
-import pandas
-import geopandas
+import numpy as numpy
 import matplotlib.pyplot as plt
-from shapely.geometry import Polygon
-import pyproj
-import warnings
-warnings.filterwarnings("ignore")
-
-#plt.rcParams['figure.figsize']=(12,10)
-#plt.rcParams['font.size']=12
-
-fixed = 'https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/'
-url = '{}{}'.format(fixed,'lfso_21jsat01')
-metadata = requests.get(url).json()
-print(metadata['label'])
-data = pandas.Series(metadata['value']).rename(index=int).sort_index()
-n = 1 # Initialize the result to 1
-for num in metadata['size']:
-  n *= num
-data = data.reindex(range(0,n),fill_value=0)
-structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
-data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
-mydata = data.reset_index()
-mydata = mydata[mydata.sex=='Total']
-mydata = mydata[mydata['c_birth']=='Total']
-mydata = mydata[mydata['isced11']=='All ISCED 2011 levels']
-mydata = mydata[mydata.age=='From 25 to 74 years']
-mydata = mydata[(mydata['lev_satis']=='Total')|(mydata['lev_satis']=='High')]
-mydata = mydata[['geo','lev_satis',0]]
-mydata.rename(columns={'geo':'ADMIN'},inplace=True)
-mydata.rename(columns={0:'Thousand persons'},inplace=True)
-mydata = mydata.pivot(index='ADMIN',columns='lev_satis',values='Thousand persons').reset_index()
-mydata['percentage'] = 100*mydata['High']/mydata['Total']
-table = mydata[['ADMIN','percentage']]
-
-world = geopandas.read_file('/content/LOP/ne_110m_admin_0_countries.zip')[['ADMIN','geometry']]
-polygon = Polygon([(-25,35),(40,35),(40,75),(-25,75)])
-europe = geopandas.clip(world,polygon)
-
-mydata = mydata.merge(europe,on='ADMIN',how='right')
-mydata = geopandas.GeoDataFrame(mydata,geometry='geometry')
-fig,ax = plt.subplots(1,figsize=(10,10))
-mydata.plot(column='percentage',alpha=0.8,cmap='cool',ax=ax,legend=True)
-ax.set_title('Percentage of employees from 25 to 74 years\nhighly satisfied with their job in 2021 (source: Eurostat)')
-ax.axis('off')
+plt.figure(figsize=(8,6))
+L = numpy.linspace(1,28,100)
+Q = L**0.25
+plt.plot(L,Q)
+x0 = 16
+y0 = 16**0.25
+m = 0.25*16**(-0.75)
+plt.plot(x0,y0,color='red',marker='o')
+plt.plot([x0,x0],[1,y0],linestyle='--',color='red')
+plt.plot([-1,x0],[y0,y0],linestyle='--',color='red')
+x = numpy.linspace(0,28,100)
+plt.plot(x,m*x-x0*m+y0,color='green')
+plt.axhline(1,color='black',linewidth=0.5)
+plt.axvline(0,color='black',linewidth=0.5)
+plt.xlabel('Labor (L)')
+plt.title('Q (K = 1)')
+plt.box(False)
+plt.xticks(range(0,28,2))
+plt.yticks(numpy.arange(1,2.5,0.25))
+plt.grid(True)
+plt.show()
