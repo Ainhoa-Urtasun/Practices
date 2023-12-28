3349030e8ae7b317dc74d4efcb02c8064862e448
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 12:57:15 +0100

    Update LOP 4-1.py

--- LOP 4-1.py
+++ LOP 4-1.py
@@ -2,29 +2,31 @@ import matplotlib.pyplot as plt
 import numpy
 
 # Data for the stacked horizontal bar chart
-tasks = ['Serve as a link between\nmanagement and employees','Advise managers on\norganizational policy matters']
+tasks = ['Serve as a link between\nmanagement and employees',
+         'Advise managers on\norganizational policy matters',
+         'Analyze and modify compensation\nand benefits policies',
+         'Perform difficult staffing duties\nincluding firing employees',
+         'Represent organization at personnel-related events']
 
-skills = 78*3
-skill1 = (78/skills)*numpy.array([95,92])
-skill2 = (78/skills)*numpy.array([95,92])
-skill3 = (78/skills)*numpy.array([95,92])
-
-colors = ['red','green','blue']  # Colors for each skill
+colors = ['red','green','blue','yellow','grey']  # Colors for each skill
 
 # Create the stacked horizontal bar chart
 plt.figure(figsize=(15,2))  # Set figure size
-plt.barh(tasks,skill1.astype(int),color=colors[0])
-plt.barh(tasks,skill2.astype(int),left=skill1.astype(int), color=colors[1])
-plt.barh(tasks,skill3.astype(int),left=[skill1[i].astype(int) + skill2[i].astype(int) for i in range(len(skill1))], color=colors[2])
+plt.barh(tasks,[3,3,1,1,1],color=colors[0])
+plt.barh(tasks,[3,3,3,3,3],left=[3,3,1,1,1], color=colors[1])
+plt.barh(tasks,[3,3,1,2,3],left=numpy.array([3,3,1,1,1])+numpy.array([3,3,3,3,3]),color=colors[2])
+plt.barh(tasks,[1,3,3,3,1],left=numpy.array([3,3,1,1,1])+numpy.array([3,3,3,3,3])+mumpy.array([3,3,1,2,3]),color=colors[3])
+plt.barh(tasks,[1,1,3,1,1],left=numpy.array([3,3,1,1,1])+numpy.array([3,3,3,3,3])+mumpy.array([3,3,1,2,3])+numpy.array([1,3,3,3,1]),color=colors[4])
 plt.yticks(tasks,fontsize=12)
 # Create a legend for the colors
-plt.legend(['Active listening','Management of Personnel Resources','Speaking'],fontsize=12,loc='upper left',bbox_to_anchor=(1.02,1.0))
+plt.legend(['Active listening','Management of Personnel Resources','Speaking','Judgement and Decision Making','Reading Comprehension'],
+           fontsize=12,loc='upper left',bbox_to_anchor=(1.02,1.0))
 
 ticks = numpy.arange(1,101)  # 100 ticks
 labels = ['' if i % 5 != 0 else str(i) for i in ticks]
 plt.xticks(ticks,labels,fontsize=12)
 plt.xlabel('Minutes of daily work')
 plt.grid(axis='x')
-plt.title('11-3121 Human Resource Manager',fontsize=16)
+plt.title('11-3121 Human Resource Managers',fontsize=16)
 plt.box(False)
 plt.show()
