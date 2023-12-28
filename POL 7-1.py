bde1f10f48fe0c24963cd43a2947a5b861200b06
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 16:52:25 +0100

    Update POL 7-1.py

--- POL 7-1.py
+++ POL 7-1.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy
 fig = plt.figure(figsize=(10,5),dpi=100)
-plt.plot(['December 2023','February 2024'],[0,7],marker='o',color='red',label='Trainees')
-plt.plot(['December 2023','February 2024'],[5,10],marker='o',color='blue',label='Control')
-plt.plot(['December 2023','February 2024'],[0,0+10-5],marker='o',color='green',ls='-.',label='Counterfactual')
+period = ['Pre-Training','Post-Training']
+plt.plot(period,[0,7],marker='o',color='red',label='Trainees')
+plt.plot(period,[5,10],marker='o',color='blue',label='Control')
+plt.plot([period,[0,0+10-5],marker='o',color='green',ls='-.',label='Counterfactual')
 plt.xticks(fontsize=14)
 ticks = numpy.arange(0,11)  # 20 ticks
 labels = ['' if i % 1 != 0 else str(i) for i in ticks]
