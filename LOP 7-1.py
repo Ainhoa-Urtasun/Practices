9e94429a36c45fa4fbd0d080eff4bbab6cfe2b55
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 08:38:22 +0100

    Update LOP 7-1.py

--- LOP 7-1.py
+++ LOP 7-1.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy
 fig = plt.figure(figsize=(10,7),dpi=100)
-plt.plot(['Before the training','After the training'],[45,87],color='red',label='Trainees')
-plt.plot(['Before the training','After the training'],[75,90],color='blue',label='Control')
-plt.plot(['Before the training','After the training'],[45,45+90-75],color='blue',ls='-.',label='Counterfactual')
-plt.title('Training Evaluation')
-plt.xlabel('Time')
-plt.ylabel('Labor productivity')
+plt.plot(['December 2023','February 2024'],[45,87],color='red',label='Trainees')
+plt.plot(['December 2023','February 2024'],[75,90],color='blue',label='Control')
+plt.plot(['December 2023','February 2024'],[45,45+90-75],color='blue',ls='-.',label='Counterfactual')
+plt.title('Training Evaluation',fontsize=16)
+plt.xlabel('Time',fontsize=14)
+plt.ylabel('Labor productivity',fontsize=14)
+plt.text('Training takes place during January 2024)
 ticks = numpy.arange(40,101)  # 100 ticks
 labels = ['' if i % 5 != 0 else str(i) for i in ticks]
 plt.yticks(ticks,labels,fontsize=12)
