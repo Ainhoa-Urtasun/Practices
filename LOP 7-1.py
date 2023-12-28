5586f6d73c4e6000012d438a3a09967c2d27af18
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 08:59:13 +0100

    Update LOP 7-1.py

--- LOP 7-1.py
+++ LOP 7-1.py
@@ -3,7 +3,7 @@ import numpy
 fig = plt.figure(figsize=(10,5),dpi=100)
 plt.plot(['December 2023','February 2024'],[0,25],marker='o',color='red',label='Trainees')
 plt.plot(['December 2023','February 2024'],[15,30],marker='o',color='blue',label='Control')
-plt.plot(['December 2023','February 2024'],[0,0+30-15],marker='o',color='blue',ls='-.',label='Counterfactual')
+plt.plot(['December 2023','February 2024'],[0,0+30-15],marker='o',color='green',ls='-.',label='Counterfactual')
 plt.title('Training Evaluation',fontsize=16)
 plt.ylabel('Labor productivity',fontsize=14)
 plt.text(0.25,-2.5,'[Training takes place during January 2024]',fontsize=14)
