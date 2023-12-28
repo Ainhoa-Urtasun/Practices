49a620ca8db5ff068d85c50dc46907fade44589e
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 16:53:02 +0100

    Update POL 7-1.py

--- POL 7-1.py
+++ POL 7-1.py
@@ -4,7 +4,7 @@ fig = plt.figure(figsize=(10,5),dpi=100)
 period = ['Pre-Training','Post-Training']
 plt.plot(period,[0,7],marker='o',color='red',label='Trainees')
 plt.plot(period,[5,10],marker='o',color='blue',label='Control')
-plt.plot([period,[0,0+10-5],marker='o',color='green',ls='-.',label='Counterfactual')
+plt.plot(period,[0,0+10-5],marker='o',color='green',ls='-.',label='Counterfactual')
 plt.xticks(fontsize=14)
 ticks = numpy.arange(0,11)  # 20 ticks
 labels = ['' if i % 1 != 0 else str(i) for i in ticks]
