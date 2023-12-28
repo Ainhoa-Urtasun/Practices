d9dd8d46dc0bbdb9a2d1a8cc984fa542a1f01001
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 08:29:35 +0100

    Update LOP 7-1.py

--- LOP 7-1.py
+++ LOP 7-1.py
@@ -7,15 +7,10 @@ plt.plot(['Before the training','After the training'],[45,45+90-75],color='blue'
 plt.title('Training Evaluation')
 plt.xlabel('Time')
 plt.ylabel('Labor productivity')
-plt.grid()
-
 ticks = numpy.arange(1,101)  # 100 ticks
 labels = ['' if i % 5 != 0 else str(i) for i in ticks]
 plt.yticks(ticks,labels,fontsize=12)
-
 plt.grid()
-
 plt.box(False)
-
 plt.legend()
 plt.show()
