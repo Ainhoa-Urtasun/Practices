ce26efff6ecfbf15e2f3c22e697daa55da910b12
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 16:49:58 +0100

    Update POL 7-1.py

--- POL 7-1.py
+++ POL 7-1.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy
 fig = plt.figure(figsize=(10,5),dpi=100)
-plt.plot(['December 2023','February 2024'],[0,25],marker='o',color='red',label='Trainees')
-plt.plot(['December 2023','February 2024'],[15,30],marker='o',color='blue',label='Control')
-plt.plot(['December 2023','February 2024'],[0,0+30-15],marker='o',color='green',ls='-.',label='Counterfactual')
+plt.plot(['December 2023','February 2024'],[0,7],marker='o',color='red',label='Trainees')
+plt.plot(['December 2023','February 2024'],[5,10],marker='o',color='blue',label='Control')
+plt.plot(['December 2023','February 2024'],[0,0+10-5],marker='o',color='green',ls='-.',label='Counterfactual')
 plt.xticks(fontsize=14)
-ticks = numpy.arange(0,32)  # 20 ticks
-labels = ['' if i % 2 != 0 else str(i) for i in ticks]
+ticks = numpy.arange(0,11)  # 20 ticks
+labels = ['' if i % 1 != 0 else str(i) for i in ticks]
 plt.yticks(ticks,labels,fontsize=12)
 plt.title('Labor Productivity (Q/L)',fontsize=16)
 plt.legend(fontsize=14)
