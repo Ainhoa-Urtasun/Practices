bc0cc7e40db9bd5f6d131b8f80e42c58c10a052f
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 08:50:29 +0100

    Update LOP 7-1.py

--- LOP 7-1.py
+++ LOP 7-1.py
@@ -3,10 +3,10 @@ import numpy
 fig = plt.figure(figsize=(10,7),dpi=100)
 plt.plot(['December 2023','February 2024'],[0,25],color='red',label='Trainees')
 plt.plot(['December 2023','February 2024'],[15,30],color='blue',label='Control')
-plt.plot(['December 2023','February 2024'],[15,15+30-15],color='blue',ls='-.',label='Counterfactual')
+plt.plot(['December 2023','February 2024'],[0,0+30-15],color='blue',ls='-.',label='Counterfactual')
 plt.title('Training Evaluation',fontsize=16)
 plt.ylabel('Labor productivity',fontsize=14)
-plt.text(0.25,0,'[Training takes place during January 2024]',fontsize=14)
+plt.text(0.25,-0.5,'[Training takes place during January 2024]',fontsize=14)
 ticks = numpy.arange(0,30)  # 100 ticks
 labels = ['' if i % 5 != 0 else str(i) for i in ticks]
 plt.yticks(ticks,labels,fontsize=12)
