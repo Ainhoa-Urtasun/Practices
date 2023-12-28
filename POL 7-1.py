42b89ad328022388e9bf94e769e2add5ae740983
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 16:29:51 +0100

    Update POL 7-1.py

--- POL 7-1.py
+++ POL 7-1.py
@@ -4,15 +4,18 @@ fig = plt.figure(figsize=(10,5),dpi=100)
 plt.plot(['December 2023','February 2024'],[0,25],marker='o',color='red',label='Trainees')
 plt.plot(['December 2023','February 2024'],[15,30],marker='o',color='blue',label='Control')
 plt.plot(['December 2023','February 2024'],[0,0+30-15],marker='o',color='green',ls='-.',label='Counterfactual')
+plt.xticks(fontsize=14)
 plt.title('Training Evaluation',fontsize=16)
 plt.ylabel('Labor productivity',fontsize=14)
 plt.text(0.25,-2.5,'[Training takes place during January 2024]',fontsize=14)
 plt.legend(fontsize=14)
+ticks = numpy.arange(0,31)
+labels = ['' if i % 5 != 0 else str(i) for i in ticks]
+plt.yticks(ticks,labels,fontsize=12)  # Define the ticks and labels
 plt.twinx()
 ticks = numpy.arange(0,31)
 labels = ['' if i % 5 != 0 else str(i) for i in ticks]
 plt.yticks(ticks,labels,fontsize=12)  # Define the ticks and labels
-plt.xticks(fontsize=14)
 plt.grid()
 plt.box(False)
 plt.show()
