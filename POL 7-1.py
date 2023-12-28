37888a6db869edac8cdf58c7dcfedb07ae997afe
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 16:44:28 +0100

    Update POL 7-1.py

--- POL 7-1.py
+++ POL 7-1.py
@@ -5,7 +5,7 @@ plt.plot(['December 2023','February 2024'],[0,25],marker='o',color='red',label='
 plt.plot(['December 2023','February 2024'],[15,30],marker='o',color='blue',label='Control')
 plt.plot(['December 2023','February 2024'],[0,0+30-15],marker='o',color='green',ls='-.',label='Counterfactual')
 plt.xticks(fontsize=14)
-ticks = numpy.arange(0,30)  # 20 ticks
+ticks = numpy.arange(0,32)  # 20 ticks
 labels = ['' if i % 2 != 0 else str(i) for i in ticks]
 plt.yticks(ticks,labels,fontsize=12)
 plt.title('Training Evaluation',fontsize=16)
