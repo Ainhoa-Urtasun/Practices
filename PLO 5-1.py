ef9cac8d728d001024f84245d43952d339902ec4
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 18:20:23 +0100

    Update PLO 5-1.py

--- PLO 5-1.py
+++ PLO 5-1.py
@@ -2,4 +2,4 @@ import numpy
 TM = numpy.array([[0.5,0.3,0],[0.1,0.6,0.1],[0.1,0.2,0.7]])
 L2023 = numpy.array([100,121,43])
 print('TM'.format(TM))
-print("L2023" + L2023)
+print('L2023'.format(L2023))
