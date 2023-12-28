0e830fc75ba181743ef9206997e6f9b14d1da01e
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 08:35:18 +0100

    Update LOP 7-1.py

--- LOP 7-1.py
+++ LOP 7-1.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy
-fig = plt.figure(figsize=(10,10),dpi=100)
+fig = plt.figure(figsize=(10,7),dpi=100)
 plt.plot(['Before the training','After the training'],[45,87],color='red',label='Trainees')
 plt.plot(['Before the training','After the training'],[75,90],color='blue',label='Control')
 plt.plot(['Before the training','After the training'],[45,45+90-75],color='blue',ls='-.',label='Counterfactual')
