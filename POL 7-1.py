a919d8ec9cae68506cdf5f1628c7f9a106783a46
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-12-28 17:16:56 +0100

    Update POL 7-1.py

--- POL 7-1.py
+++ POL 7-1.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy
 fig = plt.figure(figsize=(10,5),dpi=100)
 period = ['Pre-Training','Post-Training']
-plt.plot(period,[0,7],marker='o',color='red',label='Trainees')
-plt.plot(period,[5,10],marker='o',color='blue',label='Control')
+plt.plot(period,[0,7],marker='o',color='red',label='Trained')
+plt.plot(period,[5,10],marker='o',color='blue',label='Untrained')
 plt.plot(period,[0,0+10-5],marker='o',color='green',ls='-.',label='Counterfactual')
 plt.xticks(fontsize=14)
 ticks = numpy.arange(0,11)  # 20 ticks
 labels = ['' if i % 1 != 0 else str(i) for i in ticks]
 plt.yticks(ticks,labels,fontsize=12)
-plt.title('Labor Productivity (Q/L)',fontsize=16)
+plt.title('Labor Productivity (LP)',fontsize=16)
 plt.legend(fontsize=14)
 plt.grid()
 plt.box(False)
