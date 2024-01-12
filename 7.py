ae54c040299a780cb76a8509595db91b5baa6aea
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-01-12 23:04:25 +0100

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,9 +1,8 @@
 import pandas as pd
-data = {'Job1(t)':[53,12,3,8,76],
-        'Job2(t)':[14,46,6,13,79],
-        'Job3(t)':[7,18,35,9,69],
-        'Out(t)':[2,5,5,0,0],
-        'Sum':[76,81,49,30,0]}
-Table = pd.DataFrame(data, index=['Job1(t-1)','Job2(t-2)','Job3(t-1)','Out(t-1)','Sum'])
+data = {'Job1(t)':[53,12,3,8],
+        'Job2(t)':[14,46,6,13],
+        'Job3(t)':[7,18,35,9],
+        'Out(t)':[2,5,5,0]}
+Table = pd.DataFrame(data, index=['Job1(t-1)','Job2(t-2)','Job3(t-1)','Out(t-1)'])
 Table.loc['Sum'] = Table.sum(axis=0)
-Table['Rows'] = Table.sum(axis=1)
+Table['Sum'] = Table.sum(axis=1)
