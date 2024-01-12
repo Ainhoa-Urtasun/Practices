4a96f7c8ccfc62d4b331dcec0fad18fc5ad61988
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-01-12 22:30:22 +0100

    Update and rename PLO 5-1.py to 7.py

---
+++ 7.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data = {'Job_{1(t)}': [1, 2, 3],
+        'Column2': [4, 5, 6],
+        'Column3': [7, 8, 9]}
+df = pd.DataFrame(data, index=['Row1', 'Row2', 'Row3'])
+
+# Display the DataFrame
+print(df)
--- PLO 5-1.py
+++
@@ -1,5 +0,0 @@
-import numpy
-TM = numpy.array([[0.5,0.3,0],[0.1,0.6,0.1],[0.1,0.2,0.7]])
-L = numpy.array([100,121,43])
-print('TM = {}'.format(TM))
-print('L = {}'.format(L))
