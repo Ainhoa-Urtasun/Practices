26ed7e0b8a814901f571595648699410a0761cc4
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-01-12 23:05:52 +0100

    Update 7.py

--- 7.py
+++ 7.py
@@ -4,5 +4,6 @@ data = {'Job1(t)':[53,12,3,8],
         'Job3(t)':[7,18,35,9],
         'Out(t)':[2,5,5,0]}
 Table = pd.DataFrame(data, index=['Job1(t-1)','Job2(t-2)','Job3(t-1)','Out(t-1)'])
-Table.loc['Sum'] = Table.sum(axis=0)
 Table['Sum'] = Table.sum(axis=1)
+Table.loc['Sum'] = Table.sum(axis=0)
+
