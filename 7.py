9b916ec6bde60169bc9fb77070c05e72879aec7e
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-01-12 22:38:40 +0100

    Update 7.py

--- 7.py
+++ 7.py
@@ -2,4 +2,4 @@ import pandas as pd
 data = {'Job1(t)': [1, 2, 3],
         'Job2(t)': [4, 5, 6],
         'Job3(t)': [7, 8, 9]}
-df = pd.DataFrame(data, index=['Job1(t-1)','Job2(t-2)','Job3(t-1)'])
+table = pd.DataFrame(data, index=['Job1(t-1)','Job2(t-2)','Job3(t-1)'])
