6ec7e8476e328893f7d6aea75b8037a07bbc904c
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-01-12 22:57:26 +0100

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,6 +1,7 @@
 import pandas as pd
-data = {'Job1(t)':[53,12,3,8],
-        'Job2(t)':[14,46,6,13],
-        'Job3(t)':[7,18,35,9],
-        'Out(t)':[2,5,5,0]}
-table = pd.DataFrame(data, index=['Job1(t-1)','Job2(t-2)','Job3(t-1)','Out(t-1)'])
+data = {'Job1(t)':[53,12,3,8,76],
+        'Job2(t)':[14,46,6,13,79],
+        'Job3(t)':[7,18,35,9,69],
+        'Out(t)':[2,5,5,0],
+        'Sum':[76,81,49,30,0]}
+table = pd.DataFrame(data, index=['Job1(t-1)','Job2(t-2)','Job3(t-1)','Out(t-1)','Sum'])
