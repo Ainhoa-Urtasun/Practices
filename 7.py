6f1e227b2ea8590ccfb6d122fe5370616adda79d
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-01-12 22:31:16 +0100

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,5 +1,5 @@
 import pandas as pd
-data = {'Job_{1(t)}': [1, 2, 3],
+data = {'Job$_{1(t)}$': [1, 2, 3],
         'Column2': [4, 5, 6],
         'Column3': [7, 8, 9]}
 df = pd.DataFrame(data, index=['Row1', 'Row2', 'Row3'])
