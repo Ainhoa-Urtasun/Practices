f2983c5a0a01444d3881e07c112429d0eb2aca7c
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-01-12 22:38:13 +0100

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,9 +1,5 @@
 import pandas as pd
-def create():
-  data = {'Job1(t)': [1, 2, 3],
-          'Job2(t)': [4, 5, 6],
-          'Job3(t)': [7, 8, 9]}
-  df = pd.DataFrame(data, index=['Job1(t-1)','Job2(t-2)','Job3(t-1)'])
-  return df
-
-create()
+data = {'Job1(t)': [1, 2, 3],
+        'Job2(t)': [4, 5, 6],
+        'Job3(t)': [7, 8, 9]}
+df = pd.DataFrame(data, index=['Job1(t-1)','Job2(t-2)','Job3(t-1)'])
