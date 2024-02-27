f91b22632bad15da44f449d8991753cbf8f06544
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-02-27 12:38:52 +0100

    Update Practice - UNIT 3.py

--- Practice - UNIT 3.py
+++ Practice - UNIT 3.py
@@ -1,9 +1,7 @@
 import pandas as pd
-data = {'job_{1(t)}':[53,12,3,8],
-        'job 2(t)':[14,46,6,13],
-        'job 3(t)':[7,18,35,9],
+data = {'job1(t)}':[53,12,3,8],
+        'job2(t)':[14,46,6,13],
+        'job3(t)':[7,18,35,9],
         'Out(t)':[2,5,5,0]}
-Table = pd.DataFrame(data, index=['Job1(t-1)','Job2(t-2)','Job3(t-1)','Out(t-1)'])
-Table['Sum'] = Table.sum(axis=1)
-Table.loc['Sum'] = Table.sum(axis=0)
+Table = pd.DataFrame(data, index=['job1(t-1)','job2(t-2)','job3(t-1)','Out(t-1)'])
 
