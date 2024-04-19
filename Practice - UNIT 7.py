1384664d24d405905cb33f5aea7ea60e6dc203a4
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-04-19 12:36:25 +0200

    Update Practice - UNIT 7.py

--- Practice - UNIT 7.py
+++ Practice - UNIT 7.py
@@ -28,18 +28,15 @@ mydata = mydata[mydata.sex=='Total']
 mydata = mydata[mydata.time=='2023-Q1']
 mydata = mydata[['geo',0]]
 mydata.rename(columns={'geo':'ADMIN'},inplace=True)
-mydata.rename(columns={0:'percentage'},inplace=True)
+mydata.rename(columns={0:'Percentage'},inplace=True)
 print(mydata)
 
 world = geopandas.read_file('/content/Practices/ne_110m_admin_0_countries.zip')[['ADMIN','geometry']]
 polygon = Polygon([(-25,35),(40,35),(40,75),(-25,75)])
 europe = geopandas.clip(world,polygon)
-
-mydata = mydata.pivot(index='ADMIN',columns='time',values=['percentage']).reset_index()
-mydata.columns = mydata.columns.droplevel(level=0)
-mydata.rename(columns={'ADMIN':'GEO'},inplace=True)
-mydata = mydata.rename_axis(columns=None)
-
-mydata1 = mydata1.merge(europe,on='ADMIN',how='right')
-mydata1 = geopandas.GeoDataFrame(mydata1,geometry='geometry')
+mydata = mydata.merge(europe,on='ADMIN',how='right')
+mydata = geopandas.GeoDataFrame(mydata,geometry='geometry')
 fig,ax = plt.subplots(1,figsize=(10,10))
+mydata.plot(column='Percentage',alpha=0.8,cmap='Greens',ax=ax,legend=True)
+ax.set_title('Renewable energy sources in electricity, 2021')
+ax.axis('off')
